c603648cf42a00b12b9fe55823f477735d7cbffb
niklas.pratscher <niklas.pratscher@student.tugraz.at>
2023-12-15 18:34:34 +0100

    Filled in everything, created infinite loop

--- assignment_2/code_skeleton/ex1_policy_iteration.py
+++ assignment_2/code_skeleton/ex1_policy_iteration.py
@@ -37,47 +37,44 @@ class PolicyIteration:
         :return:
         """
 
-        # P_pi[s', s] = P(s' | s) when acting according to self.policy
+        # P_pi[s', s] = P(s' | s) when acting according to policy `self.policy`
         P_pi = np.sum(self.P * self.policy, axis=-1)
 
         # r_pi[s] = expected immediate reward in state s when acting according to self.policy
         r_pi = np.sum(self.r * self.policy, axis=-1)
 
-        v = np.zeros(self.num_states)
-        while True:
-            delta = 0
+        v = np.zeros(self.num_states, self.num_actions)
+        v_old = np.copy(v)  # Initialize v_old with v
 
-            for s in range(self.num_states):
-                old_v = v[s]
-                print(s)
-                # Update v[s] using Bellman Expectation Equation for state s
-                v[s] = r_pi[s] + gamma * np.sum(P_pi[s] * v)
-                delta = max(delta, abs(old_v - v[s]))
-                print(f"Delta: {delta}")
-                print(f"Theta: {theta}")
+        delta = 1e9  # Set initial delta
+        while delta > theta:
 
-            if delta < theta:
-                break
+            # Update the state-value function v
+            for state in range(self.num_states):
+                for action in range(self.num_actions):
+                    # Calculate the expected value of taking action a in state s
+                    value = r_pi[state] + gamma * np.dot(P_pi[state], v)
+                    v[state] += self.policy[state, action] * value
+
+            # Update delta for convergence check
+            delta = np.max(np.abs(v - v_old))
+
+            # Update v_old for next iteration
+            v_old = np.copy(v)
 
         return v
 
     def compute_Q_from_v(self, v, gamma=1.):
         """
-        Compute the Q values (i.e., a 2D-array) from
-        the state-value function `v` and return Q.
+        Convert the state-value function `v` to Q values.
         :param v: state-value function
         :param gamma: discount factor
         :return: Q values where Q[s, a] corresponds to the Q-value of taking action a in state s
         """
 
+        # TODO: convert v function to Q function
         # Hint: You'll need the MDP dynamics stored in self.P and self.r
-        Q = np.zeros((self.num_states, self.num_actions))
-
-        for s in range(self.num_states):
-            for a in range(self.num_actions):
-                # Calculate Q-value for each action in each state using the Bellman equation
-                Q[s, a] = np.sum(self.P[:, s, a] * (self.r[:, a] + gamma * np.dot(self.P[:, s, a], v)))
-
+        Q = np.matmul(self.P, v) + self.r
         assert Q.shape == (self.num_states, self.num_actions)
         return Q
 
@@ -90,27 +87,26 @@ class PolicyIteration:
         """
 
         self.policy = np.ones((self.num_states, self.num_actions)) / self.num_actions  # uniform policy
-        print(f"Gamma: {gamma}")
-        print("=============================")
+
         while True:
             policy_old = self.policy.copy()
             v = self.policy_evaluation(gamma)
             Q = self.compute_Q_from_v(v, gamma)
-            #  print(f"Gamma: {gamma}")
 
-
-            # Improve policy by acting greedily w.r.t. Q
-            for s in range(self.num_states):
-                # Choose the action that maximizes the Q-value for each state
-                best_action = np.argmax(Q[s])
+            # Improve policy (i.e., create a new one) by acting greedily w.r.t. Q
+            for state in range(self.num_states):
+                # Select the action with the highest Q-value
+                best_action = np.argmax(Q[state])
 
                 # Update the policy to select this action with probability 1
-                self.policy[s] = np.array([0 if action != best_action else 1 for action in range(self.num_actions)])
+                self.policy[state] = np.array([0 if action != best_action else 1 for action in range(self.num_actions)])
 
                 # Enforce stochasticity to avoid deterministic policies
-                self.policy[s] /= np.sum(self.policy[s])
-            if np.array_equal(policy_old, self.policy):
-                break
+                self.policy[state] /= np.sum(self.policy[state])
+
+                # Check for policy convergence
+                if np.array_equal(policy_old, self.policy):
+                    break
 
         return self.policy, v, Q
 
